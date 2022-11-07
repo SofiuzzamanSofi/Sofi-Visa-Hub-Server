2d28e4501fe6601f59a67fbedfa37d09498598a7
SofiuzzamanSofi <sofiuzzaman.sf@gmail.com>
2022-11-08 01:09:32 +0600

    mongodb connected and ready for word

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+require("colors");
+require('dotenv').config();
+
+//middleware -------
+app.use(cors());
+app.use(express.json());
+
+
+// mongodb -----
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.VISA_USER}:${process.env.VISA_SECRET}@cluster0.zwgt8km.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+app.get("/", (req, res) => {
+    res.send("Bismillahir Rahmainr Rahim, from:- Sofi Visa Hub");
+});
+
+app.listen(port, () => {
+    console.log(`Sofi Visa Hub Server Runnig on Port:${port}.`.bgCyan)
+})
