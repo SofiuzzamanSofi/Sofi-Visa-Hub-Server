25d637a6f93d5399d192364abe2499057791df95
SofiuzzamanSofi <sofiuzzaman.sf@gmail.com>
2022-11-10 00:53:32 +0600

    server json added on mongo db

--- README.md
+++ README.md
@@ -3,21 +3,36 @@ Site Name:-
 
 Website Type- Server-Side
 
-(Live Site Link [Sofi-Visa-Hub-Server](https://sofi-visa-hub.web.app/).)
+Live Site Link:- [Sofi-Visa-Hub-Server](https://sofi-visa-hub-server.vercel.app/)
 
 
 ### `## About this website-`
 
-<li></li>
+<li>Solved to Passport Problem. </li>
+<li>Helping in getting citizenship. </li>
+<li>Helping Students to go for Higher Study with discount. </li>
+<li>Working permit from Europe Uk. USA country. </li>
+<li>Working with money back guaranty. </li>
+<li>Hassle Free working. </li>
+<li>Bangladeshi Workers have special discount</li>
 
 
 
 
 ### Client Side Link-
 Live site- https://sofi-visa-hub.web.app
+
 Github- https://github.com/Porgramming-Hero-web-course/b6a11-service-review-client-side-SofiuzzamanSofi
 
 ### `Components Using This Projects--`
 
 
-<li></li>
+<li>NODE JS</li>
+<li>EXPRESS</li>
+<li>CORS</li>
+<li>DOT ENV</li>
+<li>MONGODB</li>
+
+
+### `Server--`
+<li>MongoDB</li>
--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 require("colors");
 require('dotenv').config();
-
+const service = require('./services.json');
+// console.log(service)
 //middleware -------
 app.use(cors());
 app.use(express.json());
@@ -16,11 +17,37 @@ const uri = `mongodb+srv://${process.env.VISA_USER}:${process.env.VISA_SECRET}@c
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+async function run() {
+
+    try {
+        app.get("/", (req, res) => {
+            res.send("Bismillahir Rahmainr Rahim, from:- Sofi Visa Hub, SERVER-SIDE");
+        });
+
+        app.get("/services", (req, res) => {
+            res.send({
+                success: true,
+                message: "Successfully got services data",
+                data: service
+            });
+        });
+    }
+    catch (error) {
+        console.log(`error from under function try> catch ${error}`);
+        res.send({
+            success: false,
+            error: error?.message || "error from try> catch "
+        })
+    };
 
 
-app.get("/", (req, res) => {
-    res.send("Bismillahir Rahmainr Rahim, from:- Sofi Visa Hub, SERVER-SIDE");
-});
+
+
+};
+
+run().catch(error => console.log(`error from run function catch: ${error}`.bgYellow))
+
+
 
 app.listen(port, () => {
     console.log(`Sofi Visa Hub Server Runnig on Port:${port}.`.bgCyan)
